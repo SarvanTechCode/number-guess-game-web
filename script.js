e30ffc84066a7e3c9de222c24e6cf99576d07e26
SarvanTechCode <sarvan@sarvans-MacBook-Air.local>
2024-09-02 19:13:52 +0530

    initilized for highscore value and refactoring the code and added function if repeted queryparameters is there

--- script.js
+++ script.js
@@ -5,40 +5,34 @@ let score = 20;
 let highScore = 0;
 document.querySelector(`.number`).textContent = '?';
 document.querySelector(`.score`).textContent = score;
-
+const displayMessage = function (message) {
+  document.querySelector(`.message`).textContent = message;
+};
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   if (!guess) {
-    document.querySelector(`.message`).textContent = '⛔️ please enter number';
+    displayMessage('⛔️ please enter number');
   } else if (guess === specialNumber) {
-    // document.querySelector(`.number`).textContent = specialNumber;
-    document.querySelector(`.message`).textContent = '😇 correct  numebr';
+    displayMessage('😇 correct  numebr');
     document.querySelector('body').style.backgroundColor = 'green';
     document.querySelector('.number').style.width = '30rem';
     document.querySelector(`.number`).textContent = specialNumber;
     if (highScore < score) {
       highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
     }
-  } else if (guess < specialNumber) {
+  } else if (guess !== specialNumber) {
     if (score > 1) {
       score--;
       document.querySelector(`.score`).textContent = score;
 
-      document.querySelector(`.message`).textContent =
-        '😇 your number is to low';
+      displayMessage(
+        guess > specialNumber
+          ? '😇 your number is to high'
+          : '😇 your number is to low'
+      );
     } else {
-      document.querySelector(`.message`).textContent = '😇 you lost the game';
-      document.querySelector(`.score`).textContent = 0;
-    }
-  } else if (guess > specialNumber) {
-    if (score > 1) {
-      score--;
-      document.querySelector(`.score`).textContent = score;
-
-      document.querySelector(`.message`).textContent =
-        '😇 your number is to high';
-    } else {
-      document.querySelector(`.message`).textContent = '😇 you lost the game';
+      displayMessage('😇 you lost the game');
       document.querySelector(`.score`).textContent = 0;
     }
   }
@@ -47,7 +41,7 @@ document.querySelector('.again').addEventListener('click', function () {
   specialNumber = Math.trunc(Math.random() * 20) + 1;
   score = 20;
   document.querySelector(`.score`).textContent = score;
-  document.querySelector(`.message`).textContent = 'Start guessing...';
+  displayMessage('Start guessing...');
   document.querySelector('.guess').value = '';
   document.querySelector('body').style.backgroundColor = 'rgb(34,34,34)';
   document.querySelector('.highscore').textContent = highScore;
